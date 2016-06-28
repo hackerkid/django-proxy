a00e1495be5593ec388bec938fd1d6fe14d825cb
Vishnu Ks <hackerkid@vishnuks.com>
2016-06-29 02:33:40 +0530

    Show subdomain.

--- yoda/views.py
+++ yoda/views.py
@@ -2,4 +2,6 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 def hello(request):
     domain = request.META.get('HTTP_HOST') or request.META.get('SERVER_NAME')
-    return HttpResponse('You are in' + domain)
+    pieces = domain.split('.')
+    subdomain = ".".join(pieces[:-2]) # join all but primary domain
+    return HttpResponse('You are in ' + subdomain)
